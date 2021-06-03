1414fe772cb70aef1d8c25a14f1acf768e232369
yangpengfei <yangpengfei@kaikeba.com>
2021-06-03 11:16:32 +0800

    update files

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, redirect, url_for, make_response
+from flask import Flask, request, redirect, url_for, make_response, Blueprint
 from flask import render_template
 from common.mysql_operate import db
 
@@ -10,6 +10,7 @@ def hello_world():
     name = request.cookies.get('username')
     sql = "SELECT * FROM article"
     data = db.select_db(sql)
+
     if data:
         return render_template('index.html', data=data, username=name)
     return render_template('index.html', data='yang')
@@ -33,11 +34,17 @@ def hello_home():
 @app.route('/loginout')
 def login_out():
     resp = make_response(redirect(url_for('hello_world')))
-    resp.set_cookie('username', '')
+    resp.delete_cookie('username')
     return resp
 
 
+@app.route('/wait')
+def wait():
+    return render_template('index.html')
+
+
 if __name__ == '__main__':
+
     # 参数 host port debug options
     # host 要监听的主机名 默认 127.0.0.1
     # port 端口  默认 500
